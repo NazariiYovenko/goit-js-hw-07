a5b485e14fb2af256b5a9eb2eabbdeea8456eecc
NazariiYovenko <nazarjovenko@gmail.com>
2022-12-15 17:22:57 +0200

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+// import * as basicLightbox from "basiclightbox";
 // Change code below this line
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", onGalleryElClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+function onGalleryElClick(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  event.preventDefault();
+  const bigGalleryImg = event.target.dataset.source;
+  console.log(bigGalleryImg);
+  const instance = basicLightbox.create(`
+    <img src="${bigGalleryImg}" width="800" height="600">`);
+
+  instance.show();
+}
